d09e2d3d3ea5d9b304a881f51afbc2cbe72eec59
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-01 07:55:21 +0100

    Commit final

--- worker.js
+++ worker.js
@@ -1,13 +1,7 @@
-// Importación dentro del worker [43]
-import { MLCWorkerHandler, MLCEngine } from 'https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.49/dist/web-llm.js'; 
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
 
-// Creación del motor LLM dentro del worker [36]
-const engine = new MLCEngine(); 
+const handler = new WebWorkerMLCEngineHandler()
 
-// Creación del handler que gestionará los mensajes recibidos (onmessage) [36]
-const handler = new MLCWorkerHandler(engine);
-
-// El worker escucha mensajes del hilo principal y los pasa al handler [36]
-self.onmessage = (e) => {
-    handler.onmessage(e);
-};
+self.onmessage = (msg) => {
+  handler.onmessage(msg)
+}
